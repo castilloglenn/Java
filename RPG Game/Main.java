510994bf7fbbdf159bdcad5c370487a8cb9141a6
Glenn <55197203+castilloglenn@users.noreply.github.com>
2020-03-02 22:02:00 +0800

    Test for simulations

--- RPG Game/Main.java
+++ RPG Game/Main.java
@@ -5,11 +5,35 @@ public class Main {
     private Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Character user1 = new Character();
-        Character user2 = new Character();
-        Character user3 = new Character();
+        UserCharacter user1 = new UserCharacter();
+        UserCharacter user2 = new UserCharacter();
 
-        user1.regName();
-        user1.regClass(user1.getName());
+        ComputerCharacter enemy1 = new ComputerCharacter();
+
+        user1.regName("Glenn");
+        user1.regClass("Rogue");
+
+        user2.regName("Vlademir");
+        user2.regClass("Warrior");
+
+        for (int i = 0; i < 100; i++) {
+            user1.gainExp(100000);
+            user2.gainExp(1000000);
+        }
+
+        user1.enterBattleState();
+        user2.enterBattleState();
+
+        System.out.println("Glenn attacks Vlademir");
+
+        user2.setBattleHealth(user2.getBattleHealth() - user1.dealDamage(user1.getPhyAttack(), user2.getPhyDefense()));
+
+        user2.getStatus();
+
+        System.out.println("Vlademir counterattacks!");
+
+        user1.setBattleHealth(user1.getBattleHealth() - user2.dealDamage(user2.getPhyAttack(), user1.getPhyDefense()));
+
+        user1.getStatus();
     }
 }
